a73f1c7cec14538eaaf0d11615c64e5a2587ea08
ahmad1bakran <ahmad0bakran@gmail.com>
2026-07-05 19:13:31 +0300

    ----

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,24 @@
+#!/bin/bash
+
+set -e
+
+export NVM_DIR="$HOME/.nvm"
+source "$NVM_DIR/nvm.sh"
+
+nvm use 20
+
+export PATH="$HOME/.npm-global/bin:$PATH"
+
+echo "Node: $(node -v)"
+echo "NPM: $(npm -v)"
+echo "PM2: $(which pm2)"
+
 cd /opt/nukhbat-frontend
 
-node -v
+git fetch origin
+git reset --hard origin/main
 
-git reset --hard
-git pull origin main
-
-rm -rf node_modules .next package-lock.json
-
-npm install
+npm ci
 npm run build
 
-pm2 restart frontend
+pm2 restart frontend || pm2 start npm --name frontend -- start
